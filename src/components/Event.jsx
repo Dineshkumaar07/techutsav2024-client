228a92ea687c02e3ac80b8e3057b2ff3561f3faa
venkadesh004 <venkadeshdeveloper@gmail.com>
2024-03-03 09:50:23 +0530

    feature event details

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
+import EventDetails from "./EventDetails";
+import { Link } from "react-router-dom";
 
-const Event = ({ eventName, eventDescription, image }) => {
+import EventLogo from "../assets/event-logo.png";
+
+const Event = ({ uniqueName, eventName, eventDescription, image }) => {
   const [isSeeMoreHovered, setIsSeeMoreHovered] = useState(false);
 
   return (
-    <div className="lg:w-full w-5/6 border-2 border-black rounded-md p-7 flex flex-col gap-5 items-center cursor-pointer transition-transform duration-300 ease-in-out transform hover:scale-90">
-      <img src={image} alt="Event" className="w-1/2 md:w-1/3  mb-4" />
+    <div className="lg:w-full w-5/6 border-2 border-black rounded-md p-7 flex flex-col gap-9 items-center cursor-pointer transition-transform duration-300 ease-in-out transform hover:scale-90 aspect-[1/1] text-center">
+      <img src={EventLogo} alt="Event" className="w-1/2 md:w-1/3  mb-4" />
       <h1 className="font-bold text-3xl">{eventName}</h1>
       <h1>{eventDescription}</h1>
       <button
@@ -15,7 +19,7 @@ const Event = ({ eventName, eventDescription, image }) => {
         onMouseEnter={() => setIsSeeMoreHovered(true)}
         onMouseLeave={() => setIsSeeMoreHovered(false)}
       >
-        See More
+        <Link to={`/events/${uniqueName}`}>See More</Link>
       </button>
     </div>
   );
