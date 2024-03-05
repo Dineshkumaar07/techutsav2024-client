affbcf96e7c2f57867b9e871b769dd3bc0643273
venkadesh004 <venkadeshdeveloper@gmail.com>
2024-03-06 03:15:24 +0530

    fix workshop aspect-ratio

--- src/components/Flagship.jsx
+++ src/components/Flagship.jsx
@@ -7,11 +7,11 @@ import EventLogo from "../assets/event-logo.png";
 
 const Flagship = ({ uniqueName, eventName, eventDescription, image }) => {
   const [isSeeMoreHovered, setIsSeeMoreHovered] = useState(false);
-  const mobileCheck = useMediaQuery("(min-width: 600px)");
+  const mobileCheck = useMediaQuery("(min-width: 1350px)");
 
   return (
     <div
-      className={`p-9 md:w-1/2 border-2 border-black rounded-md  flex flex-col gap-9 items-center cursor-pointer transition-transform duration-300 ease-in-out transform hover:scale-90 aspect-[1/1] text-center`}
+      className={`p-9 md:w-1/2 border-2 border-black rounded-md  flex flex-col gap-9 items-center cursor-pointer transition-transform duration-300 ease-in-out transform hover:scale-90 text-center `}
     >
       <img src={EventLogo} alt="Event" className="w-1/2 md:w-1/3  mb-4" />
       <h1 className="font-bold text-3xl">{eventName}</h1>
